3af45da442197ac1986241006fcc0e038130987f
kaola526 <huweixiong@storswift.com>
2025-03-19 10:11:36 +0800

    add pow_puzzle_internal_key

--- src/rpc/mempool.rs
+++ src/rpc/mempool.rs
@@ -6,6 +6,7 @@ use axum::extract::State;
 use axum::response::Json;
 use axum::response::Response;
 use neptune_cash::rpc_server::MempoolTransactionInfo;
+use neptune_cash::rpc_server::ProofOfWorkPuzzle;
 use std::sync::Arc;
 use tarpc::context;
 
@@ -18,6 +19,20 @@ pub async fn mempool_overview(
     let mempool_transaction_infos = s
         .rpc_client
         .mempool_overview(context::current(), s.token(), start_index, number)
+        .await
+        .map_err(rpc_err)?
+        .map_err(rpc_method_err)?;
+
+    Ok(Json(mempool_transaction_infos))
+}
+
+pub async fn pow_puzzle_internal_key(
+    State(state): State<Arc<AppState>>,
+) -> Result<Json<Option<ProofOfWorkPuzzle>>, Response> {
+    let s = state.load();
+    let mempool_transaction_infos = s
+        .rpc_client
+        .pow_puzzle_internal_key(context::current(), s.token())
         .await
         .map_err(rpc_err)?
         .map_err(rpc_method_err)?;
